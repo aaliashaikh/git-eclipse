b0d3123dc019570b0ca95e0f485ad1a4949bebd8
aaliashaikh <aaliashaikh3@gmail.com>
2021-03-23 17:19:59 +0530

    adding new file calculator.java

---
+++ my-dummy-proj/src/Calculator.java
@@ -0,0 +1,15 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=8;
+		
+		int sum = a + b;
+		
+		System.out.println("Addition is " + sum);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+	}
+}
